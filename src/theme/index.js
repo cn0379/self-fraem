f737a27c5dd27fe22b8cfca9e087c8f31d333519
cn0379 <cn0379@outlook.com>
2020-10-16 23:02:27 +0800

    简单的结构

---
+++ src/theme/index.js
@@ -0,0 +1,10 @@
+/**
+ * 自定义antd主题 less样式
+ */
+module.exports = {
+  'primary-color': '#1e90ff',
+  'info-color': '#fdd473',
+  'success-color': '#8bf175',
+  'error-color': '#f53f49',
+  'border-radius-base': '2px'
+}
